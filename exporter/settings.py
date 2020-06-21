8ade217b91a3f1a7ddf3fbd8c0097158c5c3ae84
alex <alex@actionfi.com>
2020-06-21 15:13:29 +0530

    changing allowed_hosts

--- exporter/settings.py
+++ exporter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sowlux3_*9l=)0p6!%qj6qq5mnxp41w6-63pq$=@$6an7oucr)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["product-extractor.herokuapp.com"]
 
 
 # Application definition
