f7d118138bff42708d937b3fe6c27873d1817ec6
alex <alex@actionfi.com>
2020-06-21 16:45:30 +0530

    psql

--- exporter/settings.py
+++ exporter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sowlux3_*9l=)0p6!%qj6qq5mnxp41w6-63pq$=@$6an7oucr)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["product-extractor.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'exporter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'exporter',
+        'USER': 'my_user',
+        'PASSWORD': 'my_password',
+        'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
